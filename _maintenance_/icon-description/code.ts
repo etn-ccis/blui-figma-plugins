5b5938663cfc01eb3e7f41b015d03349d6300f5f
huayunh <hhuayun0828@gmail.com>
2021-07-08 20:49:01 -0400

    Nuke! 🤯

--- _maintenance_/icon-description/code.ts
+++ _maintenance_/icon-description/code.ts
@@ -6,11 +6,6 @@ const UPDATE_MATERIAL = true;
 
 const PXBLUE_META = 'https://raw.githubusercontent.com/pxblue/icons/master/svg/index.json';
 
-// const matJSON = require('./matMeta');
-
-import { data } from './matMeta.js';
-console.log('data!', data);
-
 type IconSet = { [name: string]: string[] };
 
 // count the icon description status
@@ -28,30 +23,34 @@ if (!figma.currentPage.selection.length) {
     // create a fake UI and send the network request
     figma.showUI(__html__, { visible: false });
     if (UPDATE_MATERIAL) {
-        console.log(data);
-        const iconSet = data;
-        // updateIcons(iconSet);
+        // couldn't find a proper way to import the json
+        // @ts-ignore
+        const iconSet = matIconSet;
+        updateIcons(iconSet);
     }
     figma.ui.postMessage({ url: PXBLUE_META, updateMaterial: UPDATE_MATERIAL });
 }
 
 function addDescriptionToIconNode(node, icons: IconSet): void {
     if (node && node.type === 'COMPONENT') {
-        if (node.description === undefined || node.description === '') {
-            iconsWithoutTags.push(node.name);
-        }
         // find a node, let's see if it is an icon
-        if (Object.keys(icons).includes(node.name)) {
+        if (icons[node.name] !== undefined) {
             // bingo, it is a recognized icon.
 
-            const newDescription = icons[node.name].join(', ');
+            // the tags are "[]" in the meta file
+            if (icons[node.name].length === 0) {
+                iconsWithoutTags.push(node.name);
+            } else {
+                // there are some tags in the meta file, add them to the description
+                const newDescription = icons[node.name].join(', ');
 
-            if (newDescription !== node.description) {
-                node.description = newDescription;
-                updateCount++;
+                if (newDescription !== node.description) {
+                    node.description = newDescription;
+                    updateCount++;
+                }
+                // remove it from the set to speed up the search, and also to avoid fights between dup names in pxb and mat icons
+                icons[node.name] = undefined;
             }
-            // remove it from the set to speed up the search, and also to avoid fights between dup names in pxb and mat icons
-            icons[node.name] = undefined;
         } else {
             // user selected something whose name cannot be recognized by the plugin
             iconsNotInMeta.push(node.name);
@@ -60,7 +59,6 @@ function addDescriptionToIconNode(node, icons: IconSet): void {
 }
 
 function updateIcons(iconSet: IconSet) {
-    console.log(iconSet);
     if (figma.currentPage.selection.length) {
         for (const node of figma.currentPage.selection) {
             addDescriptionToIconNode(node, iconSet);
@@ -77,7 +75,7 @@ function updateIcons(iconSet: IconSet) {
     }
 
     if (iconsWithoutTags.length !== 0) {
-        console.log("These icons showed up in meta files, but didn't have anything in the tags field:");
+        console.log("These icons showed up in meta files, but their 'tags' field are empty:");
         console.log(iconsWithoutTags);
     }
     if (iconsNotInMeta.length !== 0) {
--- _maintenance_/icon-description/utils/build-material.js
+++ _maintenance_/icon-description/utils/build-material.js
@@ -1,6 +1,9 @@
 const fs = require('fs');
 const fetch = require('node-fetch');
 
+// everything works fine on the PXB side, which uses GitHub's API.
+// But Material Design's meta json file has CORS restrictions on it, therefore we have to
+// download it on build.
 async function main() {
     fetch('https://fonts.google.com/metadata/icons')
         .then((response) => response.text())
@@ -10,7 +13,7 @@ async function main() {
             JSON.parse(data).icons.forEach((icon) => {
                 iconSet[icon.name] = icon.tags;
             });
-            fs.writeFileSync('./matMeta.ts', 'export const data = ' + JSON.stringify(iconSet));
+            fs.writeFileSync('./build/matMeta.js', 'const matIconSet = ' + JSON.stringify(iconSet));
         });
 }
 
